b74cca6337c3a0648b56cddcb9efe3f757b35c12
Lucas Emanuel Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2025-08-06 18:13:28 -0300

    primer commit

---
+++ 01-hola-mundo.php
@@ -0,0 +1,16 @@
+<?php 
+
+include 'includes/header.php';
+
+// Imprimir en pantalla con diferentes funciones
+echo "Hola Mundo con echo"; // Imprime texto (puede imprimir múltiples cadenas)
+print "Este es un print"; // Similar a echo, pero solo acepta una cadena
+print_r("Este es un print_r"); // Imprime estructuras (útil para arrays)
+var_dump("Este es un var_dump"); // Muestra tipo de dato, longitud y valor
+
+
+include 'includes/footer.php'; 
+?>
+
+<!-- Este código es HTML puro y no necesita ir dentro del bloque PHP -->
+<h1>Hola Mundo PHP</h1>
