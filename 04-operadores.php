315ed23aa37e69e639f68edbbf8c3b29fc588b6d
Lucas Emanuel Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2025-08-07 10:52:58 -0300

    operadores

---
+++ 04-operadores.php
@@ -0,0 +1,26 @@
+<?php include 'includes/header.php';
+
+$num1 = 20;
+$num2 = 10;
+
+//Suma 
+echo "Suma: ", $num1 + $num2; 
+echo "<br/>"; //colocamos un salto de linea
+
+//Resta
+echo "Resta: ", $num1 - $num2;
+echo "<br/>"; 
+
+//Multiplicacion
+echo "Multiplicar: ", $num1 * $num2;
+echo "<br/>"; 
+
+//Divicion
+echo "Dividir: ", $num1 / $num2;
+echo "<br/>"; 
+
+//Potencia
+echo "Potencia:", 2 ** 3;
+
+
+include 'includes/footer.php';
