82b48213fab3ad6d97926d1a86adf6d52570440d
Lucas Emanuel Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2025-08-14 10:21:41 -0300

    while, do-while, for

--- 13-loops.php
+++ 13-loops.php
@@ -1,62 +1,51 @@
 <?php include 'includes/header.php';
 
 //while
-
-// primero revisa la condicion
+// primero revisa la condición antes de entrar al bucle
 $numero = 0;
 
 while ($numero < 10)
-    {
-        echo $numero . ", "; //imprime el valor que contenga la variable
-        $numero++; //incrementa de 1 en 1
-    }
-
+{
+    echo $numero . ", "; // imprime el valor actual de la variable
+    $numero++; // incrementa de 1 en 1
+}
 
 // do while
-//ingresa al bucle primero y luego revisa la condicion
-
+// ingresa al bucle primero y luego revisa la condición
 echo "<br>";
 
 $numero2 = 0;
 
-do{
-    echo $numero2 . ", "; //imprime el valor que contenga la variable
-    $numero2++; //incrementa de 1 en 1
-  }while($numero2 < 10);
-
+do {
+    echo $numero2 . ", "; // imprime el valor actual de la variable
+    $numero2++; // incrementa de 1 en 1
+} while ($numero2 < 10);
 
 //for loop
 echo "<br>";
 
-for($numero3 = 0; $numero3 < 10; $numero3++)
+for ($numero3 = 0; $numero3 < 10; $numero3++)
 {
     echo $numero3 . ", ";
 }
 
-
 //ejemplo
-
-/* si el numero actual es 
-multiplo de 3 debe de imprimir Fizz
-si es multipli de 5 imprimir Buzz
-si es multiplo de 3 y 5 imprimir Fiz Buzz
+/* Si el número actual es:
+   - Múltiplo de 3 → imprimir Fizz
+   - Múltiplo de 5 → imprimir Buzz
+   - Múltiplo de 3 y 5 → imprimir Fizz Buzz
 */
 echo "<br>";
 
-for ($i = 1; $i < 20; $i++){
-    if($i % 3 === 0 && $i % 5 === 0)
-    {
+for ($i = 1; $i < 20; $i++) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
         echo $i . " - Fizz Buzz <br>";
-    }else if ($i % 3 === 0)
-        {
-            echo $i . " - Fizz <br>";   
-        }else if($i % 5 === 0)
-            {
-               echo $i . " - Buzz <br>" ;
-            }
+    } else if ($i % 3 === 0) {
+        echo $i . " - Fizz <br>";   
+    } else if ($i % 5 === 0) {
+        echo $i . " - Buzz <br>" ;
+    }
 }
 
-
 include 'includes/footer.php';
-
 ?>
