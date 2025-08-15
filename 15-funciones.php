971174b3fefd8a359aa2d35996679dcb7834d66a
Lucas Emanuel Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2025-08-15 11:20:31 -0300

    funciones que retornan valor

--- 15-funciones.php
+++ 15-funciones.php
@@ -23,5 +23,14 @@ echo "<br>";
 saludar("Lucas"); // Hola, Lucas
 
 
+function sumar (int $num1, int $num2){
+    echo $num1 , "<br>";
+  
+    echo $num1 + $num2, "<br>"; 
+}
+
+sumar(num2: 10, num1: 20);//paremetros nombrados
+sumar(num1: 10, num2: 20);
+
 include 'includes/footer.php';
 ?>
