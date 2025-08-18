11acc23fbde346e580846a2a5a05f52bfb8f4af6
Lucas Emanuel Dos Santos <lucas.emanuel.dossantos40@gmail.com>
2025-08-18 10:51:16 -0300

    require

--- 17-require.php
+++ 17-require.php
@@ -1,6 +1,12 @@
-<?php include 'includes/header.php';
+<?php 
+include 'includes/header.php';
 
-
-
+// require 'includes/header.php';
+// 🔹 require se usa para incluir un archivo externo en el código.
+//    A diferencia de include, si el archivo no existe o tiene un error,
+//    el script se detiene con un Fatal Error.
+//    Se recomienda usar require para archivos esenciales como configuración,
+//    conexión a base de datos o clases importantes.
 
 include 'includes/footer.php';
+?>
